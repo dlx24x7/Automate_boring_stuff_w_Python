9ac24b92855a1a9e4275a4bbc766f8d8b8eb56a0
Conrad Unciano <conradunciano@Conrads-MacBook-Pro.local>
2019-02-23 07:42:32 -0500

    Initial commit

---
+++ vampire.py
@@ -0,0 +1,14 @@
+# this code teaches you how to program
+# Its a great way to learn coding
+name = 'sam'
+age = 2001
+print(age)
+
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
